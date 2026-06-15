331a9b27696c8e06fc39d7e62e361cbc503a0908
qsebring3930 <77795412+qsebring3930@users.noreply.github.com>
2026-06-15 00:45:18 -0500

    Update fetch_apify.py
    removed drop tables for final run

--- script/fetch_apify.py
+++ script/fetch_apify.py
@@ -237,11 +237,6 @@ for item in client.dataset(run.default_dataset_id).iterate_items():
     print(item)
     update_stats(cursor, item)
 
-cursor.execute("""
-DROP TABLE race_playtime;
-DROP TABLE race_levels;
-""")
-
 conn.commit()
 print("Stats updated.")
 
