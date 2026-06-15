f3a4d39d66c8f8b9af9d90dc15f2532c06b3f56e
qsebring3930 <77795412+qsebring3930@users.noreply.github.com>
2026-06-15 01:32:03 -0500

    Update fetch_apify.py
    changed snapshot again

--- script/fetch_apify.py
+++ script/fetch_apify.py
@@ -71,7 +71,7 @@ CREATE TABLE map_playtime (
 conn.commit()
 print("Tables created or already exist.")
 
-SNAPSHOT_MINUTES = 5
+SNAPSHOT_MINUTES = 1
 
 def race_to_column(race_name):
     col = race_name.lower()
